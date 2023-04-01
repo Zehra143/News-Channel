d5eae5b4ded2f03d613fc260ed767f8996ad8393
Zehra143 <123265616+Zehra143@users.noreply.github.com>
2023-04-01 13:10:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+function changeImageOver()
+{
+    document.getElementById("1").src="https://i.postimg.cc/sgKp4Jfs/iverted-ctv.png";
+}
+function changeImageLeave()
+{
+    document.getElementById("1").src="https://i.postimg.cc/VNm2h3LK/ctv.png";
+}
+function changeImgOver()
+{
+    document.getElementById("2").src="https://i.postimg.cc/ZRMQWWvk/iverted-cnn.png";
+}
+function changeImgLeave()
+{
+    document.getElementById("2").src="https://i.postimg.cc/8z0vf2Xj/usa-cnn.png";
+}
+function changeImOver()
+{
+    document.getElementById("3").src= "https://i.postimg.cc/yd02J1Kv/inverted-bbc.png";
+}
+function changeImLeave()
+{
+    document.getElementById("3").src= "https://i.postimg.cc/GhRxcn6R/bbc-uk.jpg";
+}
+function changeIgOver()
+{
+    document.getElementById("4").src= "https://i.postimg.cc/pTV6qgnB/inverted-aaj-tak.png";
+}
+function changeIgLeave()
+{
+    document.getElementById("4").src= "https://i.postimg.cc/zDgxzjBC/aaj-tak-india.jpg";
+}
+function changeIOver()
+{
+    document.getElementById("5").src= "https://i.postimg.cc/HWX6bYKC/inverted-abc.png";
+}
+function changeILeave()
+{
+    document.getElementById("5").src="https://i.postimg.cc/0yd38CVW/australia.jpg";
+}
+function changeImgeOver()
+{
+    document.getElementById("6").src= "https://i.postimg.cc/C1FxJLN6/inverted-kbs.png";
+}
+function changeImgeLeave()
+{
+    document.getElementById("6").src= "https://i.postimg.cc/Cxj7GLDz/korea.png";
+}
+function changeIgeOver()
+{
+    document.getElementById("7").src= "https://i.postimg.cc/pXyYqPcT/iverted-china.png";
+}
+function changeIgeLeave()
+{
+    document.getElementById("7").src="https://i.postimg.cc/zGBDW34k/china.png";
+}
+function changeImaeOver()
+{
+    document.getElementById("8").src="https://i.postimg.cc/tgFLJ3FL/inverted-brazil.png";
+}
+function changeImaeLeave()
+{
+    document.getElementById("8").src="https://i.postimg.cc/9MvwgyHV/brazil.png";
+}
+function changeIagOver()
+{
+    document.getElementById("9").src= "https://i.postimg.cc/wvQMXsYr/invetrted-24.png";
+}
+function changeIagLeave()
+{
+    document.getElementById("9").src= "https://i.postimg.cc/63R6hkHY/ujraine.png";
+}
+function changeImagOver()
+{
+    document.getElementById("10").src="https://i.postimg.cc/kXbrLPBJ/inverted-aljazeera.png";
+}
+function changeImagLeave()
+{
+    document.getElementById("10").src= "https://i.postimg.cc/VspW2bWJ/qatar.png";
+}
